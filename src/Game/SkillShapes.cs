cd7a66dfc01227c5199957ec213383efa2e5fa0a
FeldrinH <fhandswitch@gmail.com>
2016-01-29 16:21:37 +0200

    added SkillShapes

---
+++ src/Game/SkillShapes.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Xna.Framework;
+using Newtonsoft.Json.Linq;
+using ProjectBueno.Game.Entities;
+using System.Collections.Generic;
+
+namespace ProjectBueno.Game.Spells
+{
+	//Base Skill for Shapes
+	public abstract class SkillShape : Skill
+	{
+		protected SkillShape(JObject skill) : base(skill)
+		{
+		}
+		public abstract void generateProjectiles(Vector2 pos, List<Projectile> projectiles);
+	}
+
+	//Concrete Shapes
+	public class ShapeBall : SkillShape
+	{
+		public ShapeBall(JObject skill) : base(skill)
+		{
+		}
+
+		public override void generateProjectiles(Vector2 pos, List<Projectile> projectiles)
+		{
+			//Add projectile
+		}
+	}
+	public class ShapeBurst : SkillShape
+	{
+		public ShapeBurst(JObject skill) : base(skill)
+		{
+			partCount = (int)skill["partCount"];
+		}
+
+		protected static int partCount;
+
+		public override void generateProjectiles(Vector2 pos, List<Projectile> projectiles)
+		{
+			for (int i = 0; i < partCount; i++)
+			{
+				//Add projectiles
+			}
+		}
+	}
+	public class ShapeStream : SkillShape
+	{
+		public ShapeStream(JObject skill) : base(skill)
+		{
+			partCount = (int)skill["partCount"];
+		}
+
+		protected static int partCount;
+
+		public override void generateProjectiles(Vector2 pos, List<Projectile> projectiles)
+		{
+			for (int i = 0; i < partCount; i++)
+			{
+				//Add projectiles
+			}
+		}
+	}
+}
