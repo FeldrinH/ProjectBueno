2a9e2735d4568fa1835a5d571674824e6a6e4ae2
FeldrinH <fhandswitch@gmail.com>
2016-03-23 16:01:53 +0200

    Added PropPrejudice

--- src/Game/SkillProp.cs
+++ src/Game/SkillProp.cs
@@ -19,4 +19,32 @@ namespace ProjectBueno.Game.Spells
 
 		public const string ARCINGID = "PropArcing"; //HARDCODED EXCEPTION.(TODO)
 	}
+
+	public class PrejudiceProp : SkillProp
+	{
+		public PrejudiceProp(JObject skill) : base(skill)
+		{
+			if (skill["robots"] != null)
+			{
+				damageRobots = (float?)skill["robots"]["damage"] ?? 0.0f;
+				healRobots = (float?)skill["robots"]["heal"] ?? 0.0f;
+				controlRobots = (int?)skill["robots"]["control"] ?? 0;
+			}
+
+			if (skill["living"] != null)
+			{
+				damageLiving = (float?)skill["living"]["damage"] ?? 0.0f;
+				healLiving = (float?)skill["living"]["heal"] ?? 0.0f;
+				controlLiving = (int?)skill["living"]["control"] ?? 0;
+			}
+		}
+
+		public float healRobots;
+		public float damageRobots;
+		public int controlRobots;
+
+		public float healLiving;
+		public float damageLiving;
+		public int controlLiving;
+	}
 }
